c54d2215324b07254fcdfd7b534ef8e233a0dc3a
amda <53900519+amda-phd@users.noreply.github.com>
2022-08-29 18:15:03 +0200

    Minor changes

--- src/health/healthController.ts
+++ src/health/healthController.ts
@@ -1,9 +1,9 @@
-import { Get, Route } from "tsoa";
+import { Get, Route, Controller } from "tsoa";
 import { Health } from "./health";
 import { HealthService } from "./healthService";
 
 @Route("health")
-export class HealthController {
+export class HealthController extends Controller {
   @Get()
   public async getHealth(): Promise<Health> {
     return new HealthService().get();
